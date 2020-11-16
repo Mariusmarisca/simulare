e7446d1c6beb0fa71077d2e069f1611f063c5bb4
Mariusmarisca <70143537+Mariusmarisca@users.noreply.github.com>
2020-11-16 15:01:56 +0200

    added files

--- resources/index.js
+++ resources/index.js
@@ -1,30 +1,59 @@
 $(function(){
     const menuItem=$('nav').find('a');
     console.log('menuItem:',menuItem);
-    const galleryItem=$('.gallery-item');
+    const galleryItem=$('.items-wrapper');
     console.log('galleryItem:', galleryItem);
     const overlayItem=$('.overlay');
     const imgWrapper=$('.img-wrapper');
     const closeButton=$('.close-button')
     console.log('closeButton:', closeButton);
+    const cartWrapper=$('.cart-wrapper');
+    console.log('cartWrapper', cartWrapper);
+    const pictureSelected=$('.image-wrapper');
 
-    menuItem.click(function(e){
-        e.preventDefault();
-        menuItem.removeClass('selected');
-        $(this).addClass('selected');
-    })
+     productCoatObj = products.coats;
+     console.log('productCoatObj', productCoatObj);
 
-    galleryItem.click(function(){
-        imgWrapper.css({backgroundImage:"url(assets/" + $(this).data('img') + ")"});
-        overlayItem.fadeIn();
-    })
 
-    imgWrapper.click(function(){
-        e.stopPropagation();
-    })
+     getproductHtml=function(index,productCoatObj) {
+           return `<div data-img="coat-1.jpg" class="gallery-item product1" data-index=${index} data-id=${productCoatObj.id}>
+           <div class="image-wrapper" style="background-image: url(assets/coats/${productCoatObj.imgUrl})"></div>
+           <div class="product-details">
+               <p>${productCoatObj.name}</p>
+               <p>${productCoatObj.currency} ${productCoatObj.price}</p>
+           </div>
+       </div>
+           `
+     };
+       for (let i=0; i < products.coats.length; i++){
+           let productObj=products.coats[i];
+           productHtml=getproductHtml(i, productObj);
+           galleryItem.append(productHtml);
+       } 
+
+     galleryItem.delegate('.image-wrapper',"click", function(){
+         const id= $(this).parents('.gallery-item').data('id');
+         const index= $(this).parents('.gallery-item').data('index');
+         console.log('index', index);
+         console.log(id);
+
+         if (pictureSelected) {
+             overlayItem.fadeIn();
+         }
+ 
+         const productIndex= products.coats[index];
+          console.log("productItem", productIndex); 
+        cartWrapper.css({backgroundImage:"url(assets/coats/" + productIndex.imgUrl + ")"});
+       $('.overlay-product .name').text(productIndex.name);
+       $('.overlay-product .price').text(productIndex.currency + productIndex.price);
+       $('.overlay-product .composition').text(productIndex.composition);
+       $('.overlay-product .country').text(productIndex.country);
+       $('.overlay-product .care').text(productIndex.care);
+     })
 
     closeButton.click(function(){
         overlayItem.fadeOut();
     })
+    
 }
 )
